881d9507e4f76ed121c02a29c15d746014086831
chucky <leonardonunesdasilva93@gmail.com>
2018-11-30 20:34:19 -0300

    m

--- funcionarios/livroNode.js
+++ funcionarios/livroNode.js
@@ -1,6 +1,15 @@
 var http = require ('http')
 var url = require('url')
 
-var requisicao = function(request, reponse) {
+var server = http.createServer(function(request, reponse) {
     reponse.writeHead(200,{"Content-Type":"text/html"});
-    if(request.url == "/"){}
+    reponse.write("<h1>dados da query string</h1>");
+    var result = url.parse(request.url, true);
+    for(var key in result.query){
+        reponse.white("<h2>"+key+":"+result.query[key]+"</h2>");
+    }
+    reponse.end();
+});
+server.listen(3000,function(){
+    console.log('servidor http.')
+});
